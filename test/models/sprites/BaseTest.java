69bd60931e552b7492de9b0b182285c84d82343f
Luca97Cuomo <luca.cuomo1997@hotmail.com>
2019-12-14 20:28:04 +0100

    Added JUnit test class for base class

--- test/models/sprites/BaseTest.java
+++ test/models/sprites/BaseTest.java
@@ -17,79 +17,16 @@ import static org.junit.Assert.*;
  * @author Davide Cafaro
  */
 public class BaseTest {
+    private Base base;
     
     public BaseTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
     @Before
     public void setUp() {
+        base = new Base(0,0,20);
     }
     
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of getCurrentHealth method, of class Base.
-     */
-    @Test
-    public void testGetCurrentHealth() {
-        System.out.println("getCurrentHealth");
-        Base instance = null;
-        int expResult = 0;
-        int result = instance.getCurrentHealth();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getTotalHealth method, of class Base.
-     */
-    @Test
-    public void testGetTotalHealth() {
-        System.out.println("getTotalHealth");
-        Base instance = null;
-        int expResult = 0;
-        int result = instance.getTotalHealth();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setCurrentHealth method, of class Base.
-     */
-    @Test
-    public void testSetCurrentHealth() {
-        System.out.println("setCurrentHealth");
-        int health = 0;
-        Base instance = null;
-        instance.setCurrentHealth(health);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setTotalHealth method, of class Base.
-     */
-    @Test
-    public void testSetTotalHealth() {
-        System.out.println("setTotalHealth");
-        int health = 0;
-        Base instance = null;
-        instance.setTotalHealth(health);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
     /**
      * Test of damage method, of class Base.
@@ -97,39 +34,38 @@ public class BaseTest {
     @Test
     public void testDamage() {
         System.out.println("damage");
-        int dmg = 0;
-        Base instance = null;
-        instance.damage(dmg);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        int dmg = 10;
+        int currentHealth=base.getCurrentHealth();
+        int excpected = currentHealth - dmg;
+        base.damage(dmg);
+        
+        assertEquals("The health is not updated properly", excpected,base.getCurrentHealth());
     }
 
+      /**
+     * Tested if the health never becames negative
+     */
+    
+    @Test
+    public void testNegativeHealth() {
+        System.out.println("Negative Health");
+        int dmg = base.getCurrentHealth()+10;
+        base.damage(dmg);
+        
+        assertEquals("The health is not updated properly", 0,base.getCurrentHealth());
+    }
     /**
      * Test of isInfected method, of class Base.
      */
     @Test
     public void testIsInfected() {
         System.out.println("isInfected");
-        Base instance = null;
-        boolean expResult = false;
-        boolean result = instance.isInfected();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        int dmg = base.getCurrentHealth();
+        base.damage(dmg);
+        assertEquals("The base is not infected", true,base.isInfected());
     }
+    
 
-    /**
-     * Test of toString method, of class Base.
-     */
-    @Test
-    public void testToString() {
-        System.out.println("toString");
-        Base instance = null;
-        String expResult = "";
-        String result = instance.toString();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+ 
     
 }
