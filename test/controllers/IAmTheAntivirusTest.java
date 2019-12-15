ce611fc461109784c9e99dc3b26903d5a6b5c490
LordCatello <carmine_carratu@libero.it>
2019-12-15 20:39:09 +0100

    Added few tests to WaveManagerTest and modified already existing test

--- test/controllers/IAmTheAntivirusTest.java
+++ test/controllers/IAmTheAntivirusTest.java
@@ -5,10 +5,7 @@
  */
 package controllers;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
--- test/models/sprites/BaseTest.java
+++ test/models/sprites/BaseTest.java
@@ -5,10 +5,7 @@
  */
 package models.sprites;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
