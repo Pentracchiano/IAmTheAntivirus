7bbb4857fba67110a094cf276c80e5775f3a353e
fchiaro <52402131+fchiaro@users.noreply.github.com>
2019-12-15 18:02:49 +0100

    added comments to WaveManager class

--- src/controllers/game/WaveManager.java
+++ src/controllers/game/WaveManager.java
@@ -21,19 +21,21 @@ public class WaveManager {
     private final int yPoint;
     private final VirusFactory virusFactory;
     
+    //To generate the number of enemies in the wave
     private final static int BASE_NUMBER_VIRUSES = 4;
     private final static int NUMBER_VIRUSES_INTERVAL = 2;
     private final static int CONST_NUMBER_VIRUSES = 1;
     
+    //To choose the type of enemies in the wave
     private final static double[] SPAWN_PROBABILITIES = {0.1, 0.2, 0.7};
     
+    //To generate the spawn delay in the wave
     private final static int LOW_CONST_DELAY = 5;
     private final static int MAX_CONST_DELAY = 25;
-    private final static int RANDOM_INTERVAL_DELAY = 10;
+    private final static int RANDOM_INTERVAL_DELAY = 10; //shouldn't be constant
     
     private final static int MAX_DELAY = RANDOM_INTERVAL_DELAY + MAX_CONST_DELAY;
     private final static int MIN_DELAY = LOW_CONST_DELAY;
-    
     private final static double CONST_DELAY_DECREASE_QUANTITY = 5;
     
     public WaveManager(int xLeftLimit, int xRightLimit, int yPoint) {
@@ -63,13 +65,15 @@ public class WaveManager {
         
         Random r = new Random();
         
+        //Num virus in una wave è pari a BASE_NUMBER_VIRUSES + (CONST_NUMBER_VIRUSES
+        //+ un numero a caso in NUMBER_VIRUSES_INTERVAL)*waveNumber
         int randomNumberViruses = r.nextInt(NUMBER_VIRUSES_INTERVAL);
         int maxWaveDifficulty = getMaxWaveDifficulty(randomNumberViruses, waveNumber);
         
         List<VirusToSpawn> virusesToSpawn = new ArrayList<>();
         
         // Viruses are add to the Wave untile the maxWaveDifficulty i reached
-        System.out.println("wave_difficuly: " + maxWaveDifficulty);
+        //System.out.println("wave_difficuly: " + maxWaveDifficulty);
         while (waveDifficulty < maxWaveDifficulty) {
             double choice = r.nextDouble();
             int level = getVirusLevel(choice, waveNumber);
@@ -78,17 +82,20 @@ public class WaveManager {
             
             // delay choice
             int randomDelay = r.nextInt(RANDOM_INTERVAL_DELAY);
-            int constDelay = getConstDelay(waveNumber);
-            
-            int delay = getVirusDelay(lastDelay, randomDelay, constDelay);
-            
+            //delay's dependency from the wave is contained in this function
+            int constDelay = getConstDelay(waveNumber);  
+
+            //Relative delay between two subsequent viruses. It's needed to accumulate
+            //on lastDelay and to compute the delay coefficient
             int relativeDelay = randomDelay + constDelay;
             lastDelay += relativeDelay;
+            int delay = lastDelay;
             
             // Add a new virusToSpawn
             virusesToSpawn.add(new VirusToSpawn(virus, delay));
             
-            // delayCoeff is 1 if relativeDelay == MAX. It's 2 if relativeDelay == MIN
+            // delayCoeff is 1 if relativeDelay == MAX. It's 2 if relativeDelay == MIN.
+            // The remaining point belong to the straight line passing through these two points.
             double delayCoeff = (double) (WaveManager.MIN_DELAY - 2 * WaveManager.MAX_DELAY + relativeDelay) /  (double) (WaveManager.MIN_DELAY - WaveManager.MAX_DELAY);
             
             waveDifficulty += (int) ceil(virus.getDifficulty() * delayCoeff);
@@ -123,11 +130,7 @@ public class WaveManager {
             return level;
     }
     
-    private int getVirusDelay(int lastDelay, int randomDelay, int constDelay) {
-        int delay = lastDelay + randomDelay + constDelay;
-        
-        return delay;
-    }
+ 
     
     private int getConstDelay(int waveNumber) {
         int constDelay = MAX_CONST_DELAY - (int)(waveNumber * CONST_DELAY_DECREASE_QUANTITY);
