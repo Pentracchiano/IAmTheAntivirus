d0e040c2964d0d403b724ef81e398680f1d7f902
fchiaro <52402131+fchiaro@users.noreply.github.com>
2019-12-15 13:42:55 +0100

    Started working on delay

--- src/controllers/game/GameController.java
+++ src/controllers/game/GameController.java
@@ -63,7 +63,6 @@ public class GameController extends Controller implements Runnable {
 
         while (gameStatus.isInGame()) {
             timeCount = 0; // counts the number of cycles
-            System.out.println("I am in game");
             
             // set wave
             gameStatus.setCurrentWaveNumber(gameStatus.getCurrentWaveNumber() + 1);
@@ -73,7 +72,6 @@ public class GameController extends Controller implements Runnable {
             
             gameStatus.setInWave(true);
             
-            System.out.println(gameStatus.toString());
             while (gameStatus.isInGame() && gameStatus.isInWave() ) {
                 // System.out.println("In wave");
                 
@@ -172,9 +170,8 @@ public class GameController extends Controller implements Runnable {
     }
 
     private void checKeyCollision(Key key) {
-        Collection<Virus> aliveSpawnedViruses = wave.getAliveSpawnedViruses();
-        
-        synchronized (aliveSpawnedViruses) {
+        synchronized (this.wave) {
+            Collection<Virus> aliveSpawnedViruses = wave.getAliveSpawnedViruses();
             boolean missedViruses = true;
             for (Virus virus : aliveSpawnedViruses) {
                 if (checkCollision(key.getBounds(), virus.getBounds())) {
--- src/controllers/game/WaveManager.java
+++ src/controllers/game/WaveManager.java
@@ -27,11 +27,12 @@ public class WaveManager {
     private final static double WAVE_DIFFICULTY_MULTIPLIER = 0.5;
     private final static int VIRUS_LEVEL_INTERVAL = 3;
     private final static double[] SPAWN_PROBABILITIES = {0.2, 0.3, 0.5};
-            //new double[WaveManager.VIRUS_LEVEL_INTERVAL];
-    
     private final static double DELAY_COEFF = 1;
     private final static double PATH_COEFF = 1;
-
+    private final static int LOWEST_CONST_DELAY = 5; //5*20 = 100 ms
+    private final static int MAX_CONST_DELAY = 75; //half a second
+    private final static double CONST_DELAY_DECREASE_QUANTITY = 3;
+    
     public WaveManager(int xLeftLimit, int xRightLimit, int yPoint) {
         this.xLeftLimit = xLeftLimit;
         this.xRightLimit = xRightLimit;
@@ -46,19 +47,22 @@ public class WaveManager {
      * lower border of the application screen (not game's console screen).
      */
     public Wave getWave(int waveNumber) {
-        int maxWaveDifficulty = (int) ceil(INITIAL_WAVE_DIFFICULTY * WAVE_DIFFICULTY_MULTIPLIER * waveNumber);
+        int constantDelay; //minum spawn interval between two different viruses
+        int randomDelay; //random additive spawn interval between two different viruses
+        
+        int maxWaveDifficulty = (int) ceil(INITIAL_WAVE_DIFFICULTY * WAVE_DIFFICULTY_MULTIPLIER * waveNumber); //linear wrt wave number
         int delay = 0;
         List<VirusToSpawn> virusesToSpawn = new ArrayList<>();
         int waveDifficulty = 0;
         Random r = new Random();
         while (waveDifficulty < maxWaveDifficulty) {
+            // Virus level construction
             double guess = r.nextDouble();
-            //System.out.println("guess: " + guess); // DEBUG
             int level;
             if (guess >= 0 && guess <= SPAWN_PROBABILITIES[0]){
                 level = waveNumber;
             }
-            else if (guess > SPAWN_PROBABILITIES[0] && guess <= SPAWN_PROBABILITIES[1]){
+            else if (guess > SPAWN_PROBABILITIES[0] && guess <= (SPAWN_PROBABILITIES[0] + SPAWN_PROBABILITIES[1])){ //guess > SPAWN_PROBABILITIES[0] && guess <= SPAWN_PROBABILITIES[1]
                 level = waveNumber - 1;
             }
             else{
@@ -69,9 +73,16 @@ public class WaveManager {
             }
             Virus virus;
             virus = virusFactory.createVirus(this.xLeftLimit, this.xRightLimit, this.yPoint, level);
+            
+            //Virus delay construction
+            constantDelay = this.MAX_CONST_DELAY - (int)(waveNumber*WaveManager.CONST_DELAY_DECREASE_QUANTITY);
+            if (constantDelay < WaveManager.LOWEST_CONST_DELAY){
+                constantDelay = WaveManager.LOWEST_CONST_DELAY;
+            }
+            System.out.println("Constant delay: " + constantDelay);
+            //double delayCoeff = 
             waveDifficulty += (int) ceil(virus.getDifficulty() * DELAY_COEFF * PATH_COEFF);
-            //System.out.println(virus.toString()); // DEBUG
-            delay += r.nextInt(10);
+            delay += (r.nextInt(10) + constantDelay);
             virusesToSpawn.add(new VirusToSpawn(virus, delay));
         }
 
