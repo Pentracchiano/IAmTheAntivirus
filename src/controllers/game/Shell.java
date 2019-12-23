8552b9f2b6bca08eb384f77c6b5458837f8bc79c
mrispoli97 <mariorispoli97@gmail.com>
2019-12-23 23:12:01 +0100

    Cancel and delete buttons now work in the shell. It's now possible to delete characters written for mistake

--- src/controllers/game/Shell.java
+++ src/controllers/game/Shell.java
@@ -40,8 +40,7 @@ public class Shell extends JLabel{
                 return;
             }
         }
-        setDefault();
-        
+        setDefault();   
     }
     
     private void setDefault(){
